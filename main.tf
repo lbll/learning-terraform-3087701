24e923288d578214b0ccad9928b251d46ae4859b
lbll <glawdys.lamons@gmail.com>
2024-01-22 15:35:54 +0100

    remove unnecessary previous sg

--- main.tf
+++ main.tf
@@ -54,34 +54,3 @@ resource "aws_instance" "blog" {
     Name = "HelloWorld"
   }
 }
-
-resource "aws_security_group_rule" "blog_http_in" {
-  type        = "ingress"
-  from_port   = 80
-  to_port     = 80
-  protocol    = "tcp"
-  cidr_blocks = ["0.0.0.0/0"]
-
-  security_group_id = module.blog_sg.security_group_id
-}
-resource "aws_security_group_rule" "blog_https_in" {
-  type        = "ingress"
-  from_port   = 443
-  to_port     = 443
-  protocol    = "tcp"
-  cidr_blocks = ["0.0.0.0/0"]
-
-  security_group_id = module.blog_sg.security_group_id
-}
-
-resource "aws_security_group_rule" "blog_http_out" {
-  type        = "egress"
-  from_port   = 0
-  to_port     = 0
-  protocol    = "-1"
-  cidr_blocks = ["0.0.0.0/0"]
-
-  security_group_id = module.blog_sg.security_group_id
-}
-
-
