0a8a11fe4e86ecacafddac4fe46a9749998484ac
lbll <glawdys.lamons@gmail.com>
2024-01-22 14:41:17 +0100

    use module for sg

--- main.tf
+++ main.tf
@@ -18,6 +18,18 @@ data "aws_vpc" "default" {
   default = true
 }
 
+module "blog_sg" {
+  source  = "terraform-aws-modules/security-group/aws"
+  version = "5.1.0"
+  name = "blog_new"
+  vpc_id = data.aws_vpc.default.id
+  ingress_rules       = [ "http-80-tcp", "https-443-tcp"]
+  ingress_cidr_blocks = [ "0.0.0.0" ]
+  egress_rules        = [ "all-all" ]
+  egress_cidr_blocks  = [ "0.0.0.O" ]
+
+}
+
 resource "aws_security_group" "blog" {
   name = "blog"
   description = " Allow HTTP/S in. Allow evertyhinq out"
@@ -29,7 +41,7 @@ resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
 
-  vpc_security_group_ids = [aws_security_group.blog.id]
+  vpc_security_group_ids = [module.blog_sg.security_group_id]
   tags = {
     Name = "HelloWorld"
   }
