0261869b627c64e65e75c305524a9a51e0d7f484
lbll <glawdys.lamons@gmail.com>
2024-01-22 15:00:31 +0100

    create new vpc

--- main.tf
+++ main.tf
@@ -14,6 +14,21 @@ data "aws_ami" "app_ami" {
   owners = ["979382823631"] # Bitnami
 }
 
+module "blog_vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "dev"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+  tags = {
+    Terraform = "true"
+    Environment = "dev"
+  }
+}
+
 data "aws_vpc" "default" {
   default = true
 }
@@ -22,7 +37,7 @@ module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "5.1.0"
   name = "blog_new"
-  vpc_id = data.aws_vpc.default.id
+  vpc_id = module.blog_vpc.vpc_id
   ingress_rules       = [ "http-80-tcp", "https-443-tcp"]
   ingress_cidr_blocks = [ "0.0.0.0/0" ]
   egress_rules        = [ "all-all" ]
@@ -33,7 +48,7 @@ module "blog_sg" {
 resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
-
+  subnet_id = module.blog_vpc.public_subnets[0]
   vpc_security_group_ids = [module.blog_sg.security_group_id]
   tags = {
     Name = "HelloWorld"
